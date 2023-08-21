b3e8afb2934b5f4f5932fd76a3bfd207e8258d2a
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-08-21 19:56:54 +0300

    Refactoring Footer component

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -4,29 +4,32 @@ import {Logo} from "../../components/logo/Logo";
 import {FlexWrapper} from "../../components/FlexWrapper";
 import {Social} from "../../components/social/Social";
 import {Menu} from "../../components/menu/Menu";
+import {Container} from "../../components/Container";
 
 const menuItems = ['Home', 'Tech Stack', 'Projects', 'Contact']
 
 export const Footer = () => {
     return (
         <StyledFooter>
-            <FooterTop>
-                <FlexWrapper justify={'space-between'} align={'center'}>
-                    <Logo section={'footer'}/>
-                    <FlexWrapper justify={'space-between'} align={'center'} columnGap={'63px'}>
-                        <Phone href={'tel:+79323161334'}>+7(932)316-13-34</Phone>
-                        <Email href={'mailto:astemirow@mail.ru'}>astemirow@mail.ru</Email>
-                        <Social section={'footer'}/>
-                    </FlexWrapper>
+            <Container>
+                <FooterTop>
+                    <FlexWrapper justify={'space-between'} align={'center'}>
+                        <Logo section={'footer'}/>
+                        <FlexWrapper justify={'space-between'} align={'center'} columnGap={'63px'}>
+                            <Phone href={'tel:+79323161334'}>+7(932)316-13-34</Phone>
+                            <Email href={'mailto:astemirow@mail.ru'}>astemirow@mail.ru</Email>
+                            <Social section={'footer'}/>
+                        </FlexWrapper>
 
-                </FlexWrapper>
-            </FooterTop>
-            <FooterBottom>
-                <FlexWrapper justify={'space-between'} align={'center'}>
-                    <Menu menuItems={menuItems}/>
-                    <Text>Designed and built by <span>Arslanbek Astemirov</span></Text>
-                </FlexWrapper>
-            </FooterBottom>
+                    </FlexWrapper>
+                </FooterTop>
+                <FooterBottom>
+                    <FlexWrapper justify={'space-between'} align={'center'}>
+                        <Menu menuItems={menuItems}/>
+                        <Text>Designed and built by <span>Arslanbek Astemirov</span></Text>
+                    </FlexWrapper>
+                </FooterBottom>
+            </Container>
         </StyledFooter>
     );
 };
