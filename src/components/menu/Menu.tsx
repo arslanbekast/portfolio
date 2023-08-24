ba707e0bc881ced4f10a6ee2ae8da9d489e64123
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-08-24 17:02:11 +0300

    Stylized menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
 import styled from "styled-components";
+import {theme} from "../../styles/Theme";
 
-export const Menu = (props: {menuItems: Array<string>}) => {
+type MenuPropsType = {
+    menuItems: Array<string>,
+    fontColor?: string,
+    fontSize?: string,
+    fontWeight?: string
+}
+
+export const Menu = (props: MenuPropsType) => {
     return (
-        <StyledMenu>
-            <ul>
+        <StyledMenu fontColor={props.fontColor}
+                    fontSize={props.fontSize}
+                    fontWeight={props.fontWeight}>
+            <ul role={'menu'} aria-label={'Меню'}>
                 {props.menuItems.map((item, i)=>{
-                    return <li key={i}><a href="">{item}</a></li>
+                    return <li key={i} role={'menuitem'}><a href="">{item}</a></li>
                 })}
             </ul>
         </StyledMenu>
     );
 };
 
-const StyledMenu = styled.nav`
+type StyledMenuPropsType = {
+    fontColor?: string,
+    fontSize?: string,
+    fontWeight?: string
+}
+
+const StyledMenu = styled.nav<StyledMenuPropsType>`
   ul {
-    list-style-type: none;
     display: flex;
     justify-content: flex-start;
     align-items: center;
     gap: 50px;
-    margin: 0;
-    padding: 0;
+    
+    
+    a {
+      color: ${props => props.fontColor || theme.colors.grey};
+      font-family: DM Sans;
+      font-size: ${props => props.fontSize || "20px"};
+      font-style: normal;
+      font-weight: ${props => props.fontWeight || "500"};
+      line-height: 26px;
+    }
   }
 `
