2eed8c0d79199ab5a6e84aa346ed4b86f9ea6d87
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-08-24 14:41:11 +0300

    Added Theme.tsx

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        primaryBg: "#fff",
+        matteBlue: "#42446E",
+        navyBlue: "#1E0E62",
+        grey: "#666",
+        black: "#000",
+    }
+}
