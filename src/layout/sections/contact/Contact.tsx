ed003093c0e41a6b38254d8a4fe93bc55bc9d4c7
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-08-25 15:30:16 +0300

    Stylized Contact component

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
 
 export const Contact = () => {
     return (
@@ -14,27 +15,20 @@ export const Contact = () => {
 };
 
 const StyledContact = styled.section`
-  background-color: #77f0ff;
   text-align: center;
+  font-family: DM Sans;
+  font-size: 58px;
+  font-weight: 700;
+  line-height: 70px;
+  letter-spacing: -1px;
 `
 
 const Title = styled.h2`
-  color: var(--colors-heading, #1E0E62);
-  //font-family: DM Sans;
+  color: ${theme.colors.navyBlue};
   font-size: 58px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 70px;
-  letter-spacing: -1px;
 `
 
 const Email = styled.a`
-  //font-family: DM Sans;
-  font-size: 58px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 70px;
-  letter-spacing: -1px;
   background: var(--gradient-heading, linear-gradient(90deg, #13B0F5 2.60%, #E70FAA 100%));
   background-clip: text;
   -webkit-background-clip: text;
