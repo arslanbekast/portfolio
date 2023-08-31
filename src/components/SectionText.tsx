871c7a00ee91f7fb2fde798689e8581e11e34d4b
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-08-31 18:11:50 +0300

    Refactoring SectionText and SectionTitle components

--- src/components/SectionText.tsx
+++ src/components/SectionText.tsx
@@ -3,11 +3,7 @@ import {theme} from "../styles/Theme";
 import {font} from "../styles/Common";
 
 export const SectionText = styled.p`
-  ${font({color: theme.colors.grey, Fmax: 32, Fmin: 26 })}
-  //color: ${theme.colors.grey};
+  ${font({color: theme.colors.grey, Fmax: 32, Fmin: 22 })}
   text-align: center;
-  //font-size: 32px;
-  //font-weight: 400;
-  //line-height: 26px;
   margin-bottom: 100px;
 `
--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -3,11 +3,7 @@ import {theme} from "../styles/Theme";
 import {font} from "../styles/Common";
 
 export const SectionTitle = styled.h2`
-  ${font({weight: 700, color: theme.colors.matteBlue, Fmax: 48, Fmin: 38 })}
-  //color: ${theme.colors.matteBlue};
+  ${font({weight: 700, color: theme.colors.matteBlue, Fmax: 48, Fmin: 32 })}
   text-align: center;
-  //font-size: 48px;
-  //font-weight: 700;
-  //line-height: 26px;
   margin-bottom: 50px;
 `
