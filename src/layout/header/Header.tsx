2c76421f2f408afe509ace2d5812c2f75b059d14
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-03 15:12:16 +0300

    Refactoring Header component

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import styled from "styled-components";
+import React, {useEffect, useState} from 'react';
 import {Logo} from "../../components/logo/Logo";
 import {DesktopMenu} from "../../components/menu/DesktopMenu";
 import {Social} from "../../components/social/Social";
@@ -11,15 +10,26 @@ import {S} from './Header_Styles'
 const menuItems = ['Home', 'Tech Stack', 'Projects', 'Contact']
 
 export const Header: React.FC = () => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const breakpoint = 768;
+
+    useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleWindowResize);
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, [])
+
     return (
         <S.Header>
             <Container>
                 <FlexWrapper justify={'space-between'} align={'center'}>
                     <Logo section={'header'}/>
                     <FlexWrapper justify={'flex-end'} align={'center'} columnGap={'50px'}>
-                        <DesktopMenu menuItems={menuItems}/>
+                        {
+                            width > breakpoint ? <DesktopMenu menuItems={menuItems}/> : <MobileMenu menuItems={menuItems}/>
+                        }
                         <Social/>
-                        <MobileMenu menuItems={menuItems}/>
                     </FlexWrapper>
                 </FlexWrapper>
             </Container>
