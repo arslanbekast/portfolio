19ec1335a8bec935ba8d305559e443ea10424075
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-03 13:18:29 +0300

    Moved StyledHeader to a separate file

---
+++ src/layout/header/Header_Styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import {theme} from "../../styles/Theme";
+
+const Header = styled.header`
+  background-color: ${theme.colors.primaryBg};
+  padding: 20px 0;
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: 99999;
+`
+
+export const S = {
+    Header
+}
