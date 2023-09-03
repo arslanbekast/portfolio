533a62705bc6e59f75d81d36009ce3b6d4f22bcc
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-03 16:34:55 +0300

    Refactoring Icon component

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -7,7 +7,7 @@ type IconPropsType = {
     height?: string
 }
 
-export const Icon = (props: IconPropsType) => {
+export const Icon: React.FC<IconPropsType> = (props: IconPropsType) => {
     return (
         <svg width={props.width || "120"} height={props.height  || "120"} viewBox={`0 0 ${props.width} ${props.height}` || "0 0 120 120"} fill="none" xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={ `${iconsSprite}#${props.iconId}` } />
