e419ec923c804ed7912482e5d2417ea065d08f4d
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-06 23:03:14 +0300

    Added GoToTop component

---
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react';
+import styled from "styled-components";
+import toTopBtn from '../../assets/images/icons/toTopBtn.png';
+import {animateScroll as scroll} from "react-scroll/modules";
+
+export const GoTopBtn = () => {
+
+    const [showBtn, setShowBtn] = useState(false);
+
+    useEffect(() => {
+       window.addEventListener('scroll', () => {
+           window.scrollY > 200 ? setShowBtn(true) : setShowBtn(false);
+       })
+    }, []);
+
+    return (
+        <>
+            {
+                showBtn && (
+                    <StyledGoTopBtn onClick={() => {scroll.scrollToTop()}}>
+                        <img src={toTopBtn} alt="Go to top button"/>
+                    </StyledGoTopBtn>
+                )
+            }
+
+        </>
+
+    );
+};
+
+const StyledGoTopBtn = styled.button`
+  background-color: transparent;
+  text-align: center;
+  border: none;
+  cursor: pointer;
+  padding: 8px;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+`
