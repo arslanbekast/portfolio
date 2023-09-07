46129f3bb4d7a10c3acabe796070ee00ce0d8719
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-07 17:30:51 +0300

    Refactoring Particle component

--- src/components/particle/Particle.tsx
+++ src/components/particle/Particle.tsx
@@ -27,10 +27,10 @@ export const Particle = () => {
                 fpsLimit: 120,
                 interactivity: {
                     events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
+                        // onClick: {
+                        //     enable: true,
+                        //     mode: "push",
+                        // },
                         onHover: {
                             enable: true,
                             mode: "repulse",
