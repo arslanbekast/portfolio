5ad966e4f55fd7bf28fd838f7285b0413fb39ead
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-05-05 16:40:59 +0300

    Change projects in Projects

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -3,23 +3,37 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {SectionText} from "../../../components/SectionText";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Project} from "./project/Project";
-import proj1 from '../../../assets/images/projects/proj1.jpeg'
-import proj2 from '../../../assets/images/projects/proj2.jpeg'
-import proj3 from '../../../assets/images/projects/proj3.jpeg'
-import proj4 from '../../../assets/images/projects/proj4.jpeg'
-import proj5 from '../../../assets/images/projects/proj5.jpeg'
-import proj6 from '../../../assets/images/projects/proj6.jpeg'
+import todo from '../../../assets/images/projects/todo.jpeg'
+import socialNetwork from '../../../assets/images/projects/social_network.jpeg'
+import cards from '../../../assets/images/projects/cards.jpeg'
 import {Container} from "../../../components/Container";
 import {S} from './Projects_Styles'
 import {Zoom} from "react-awesome-reveal";
 
 const projectsArr = [
-    {img:proj1,title:'Project Tile goes here',text:'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',techStack:'HTML , JavaScript, SASS, React'},
-    {img:proj2,title:'Project Tile goes here',text:'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',techStack:'HTML , JavaScript, SASS, React'},
-    {img:proj3,title:'Project Tile goes here',text:'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',techStack:'HTML , JavaScript, SASS, React'},
-    {img:proj4,title:'Project Tile goes here',text:'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',techStack:'HTML , JavaScript, SASS, React'},
-    {img:proj5,title:'Project Tile goes here',text:'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',techStack:'HTML , JavaScript, SASS, React'},
-    {img:proj6,title:'Project Tile goes here',text:'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',techStack:'HTML , JavaScript, SASS, React'}
+    {
+        img:cards,
+        title:'Cards',
+        text:'The app is designed for question-and-answer card-based learning, with the ability to create your own decks of cards or use existing sets.',
+        techStack:'HTML, Module SCSS, TypeScript, React, RTK Query, React Router Dom, Radix UI, React Hook Form',
+        linkPreview: 'https://cards-for-study.vercel.app/',
+        linkCode: 'https://github.com/MrEvgeniy1989/cards'
+    },
+    {
+        img:todo,
+        title:'Todolist',text:'The project provides convenient management of tasks and to-do lists through an intuitive interface, allowing you to create, edit, delete and view tasks and to-do lists.',
+        techStack:'HTML, Module CSS, TypeScript, React, Redux Toolkit, React Router Dom, Material UI, Formik',
+        linkPreview: 'https://arslanbekast.github.io/todolist/',
+        linkCode: 'https://github.com/arslanbekast/todolist'
+    },
+    {
+        img:socialNetwork,
+        title:'Social Network',
+        text:'The project is a platform where users can register, create personal profiles, view other users\' profiles, add them as friends and chat.',
+        techStack:'HTML, Module CSS, TypeScript, React, Redux, Redux Thunk, React Router Dom, Redux Form',
+        linkPreview: 'https://arslanbekast.github.io/social-network/',
+        linkCode: 'https://github.com/arslanbekast/social-network'
+    }
 ]
 
 export const Projects: React.FC = () => {
@@ -32,7 +46,7 @@ export const Projects: React.FC = () => {
                     <Zoom cascade={true} damping={0.05}>
                     {
                         projectsArr.map((item,i)=>{
-                            const {img,title,text,techStack} = item;
+                            const {img,title,text,techStack, linkPreview, linkCode} = item;
                             return (
                                 <Project
                                     key={i}
@@ -40,6 +54,8 @@ export const Projects: React.FC = () => {
                                     title={title}
                                     text={text}
                                     techStack={techStack}
+                                    linkPreview={linkPreview}
+                                    linkCode={linkCode}
                                 />
                             )
                         })
--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -16,26 +16,34 @@ type ProjectPropsType = {
 export const Project: React.FC<ProjectPropsType> = (props: ProjectPropsType) => {
     return (
         <S.Project>
-            <S.Image src={props.imgSrc} alt=""/>
-            <S.ProjectTextBox>
-                <S.Title>{props.title}</S.Title>
-                <S.Text>{props.text}</S.Text>
-                <S.StackString>Tech stack : <span>{props.techStack}</span></S.StackString>
-                <FlexWrapper justify={'space-between'} align={'center'}>
-                    <S.Link href={props.linkPreview} target='_blank'>
-                        <FlexWrapper align={'center'} columnGap={'10px'}>
-                            <Icon iconId={'link'} width={'20'} height={'20'} />
-                            <span>Live Preview</span>
-                        </FlexWrapper>
-                    </S.Link>
-                    <S.Link href={props.linkCode} target='_blank'>
-                        <FlexWrapper align={'center'} columnGap={'10px'}>
-                            <Icon iconId={'projgithub'} width={'20'} height={'20'} />
-                            <span>View Code</span>
-                        </FlexWrapper>
-                    </S.Link>
+            <FlexWrapper direction={'column'} justify={'space-between'} height={'100%'}>
+
+                <FlexWrapper direction={'column'} justify={'flex-start'}>
+                    <S.Image src={props.imgSrc} alt=""/>
+                    <S.ProjectTextBox>
+                        <S.Title>{props.title}</S.Title>
+                        <S.Text>{props.text}</S.Text>
+                        <S.StackString>Tech stack : <span>{props.techStack}</span></S.StackString>
+                    </S.ProjectTextBox>
                 </FlexWrapper>
-            </S.ProjectTextBox>
+
+                <S.ProjectFooterBox>
+                    <FlexWrapper justify={'space-between'} align={'center'}>
+                        <S.Link href={props.linkPreview} target='_blank'>
+                            <FlexWrapper align={'center'} columnGap={'10px'}>
+                                <Icon iconId={'link'} width={'20'} height={'20'} />
+                                <span>Live Preview</span>
+                            </FlexWrapper>
+                        </S.Link>
+                        <S.Link href={props.linkCode} target='_blank'>
+                            <FlexWrapper align={'center'} columnGap={'10px'}>
+                                <Icon iconId={'projgithub'} width={'20'} height={'20'} />
+                                <span>View Code</span>
+                            </FlexWrapper>
+                        </S.Link>
+                    </FlexWrapper>
+                </S.ProjectFooterBox>
+            </FlexWrapper>
         </S.Project>
     );
 };
